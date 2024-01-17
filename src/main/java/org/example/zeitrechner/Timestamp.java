f0cf62fdde4878d969de965a0a4d960975f6be1e
SimoniusGausius <simon.gaus@ict-campus.net>
2024-01-17 13:27:31 +0100

    timestamp prtotip

--- src/main/java/org/example/zeitrechner/Timestamp.java
+++ src/main/java/org/example/zeitrechner/Timestamp.java
@@ -1,15 +1,30 @@
 package org.example.zeitrechner;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Timestamp {
     private int hour;
     private int min;
     private int sek;
+    private Person person;
+    private LocalDate date;
+    public Timestamp(Person person, LocalDate date , int sek){
+        this.person=person;
+        this.sek=sek;
+        this.date=date;
+    }
 
     public void makeTimestamp() {
         LocalTime localTime = LocalTime.now();
         System.out.println(localTime);
+        this.setHour(localTime.getHour());
+        this.setMin(localTime.getMinute());
+        this.setSek(localTime.getSecond());
+    }
+    public void printTime() {
+        System.out.println("Stunde: " + this.getHour());
+        System.out.println("Minute: " + this.getMin());
+        System.out.println("Sekunde: " + this.getSek());
     }
     public int getHour() {
         return hour;
@@ -32,5 +47,6 @@ public class Timestamp {
     public static void main(String[] args) {
         Timestamp time = new Timestamp();
         time.makeTimestamp();
+        time.printTime();
     }
 }
