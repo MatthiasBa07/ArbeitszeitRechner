001473d682671a6f45bbad1f55c82e700a877363
Matthias Baumgartner <matthias.baumgartner@ict-campus.net>
2024-01-18 12:56:18 +0100

    Calculator returns

--- src/main/java/org/example/zeitrechner/Calculator.java
+++ src/main/java/org/example/zeitrechner/Calculator.java
@@ -1,68 +1,64 @@
 package org.example.zeitrechner;
 
-public class Calculator {
-    private int hour;
-    private int min;
-    private int sek;
+import java.util.ArrayList;
 
-    public void printTime() {
-        String output = new String();
-        output = "Zeit: " + this.getHour() + ":" + this.getMin() + ":" + this.getSek();
-        System.out.println(output);
+public class Calculator {
+
+    private static final Calculator calculator = new Calculator();
+
+    public static Calculator getInstance() {
+        return calculator;
     }
+
+    public String addZero(int zeit) {
+        String result;
+        if (zeit < 10) result = "0" + zeit;
+        else result = Integer.toString(zeit);
+        return result;
+    }
+
+    public void printTime(int s) {
+        int[] zeit = Calculator.getInstance().sekToTime(s);
+        String[] result = new String[]{Calculator.getInstance().addZero(zeit[0]), Calculator.getInstance().addZero(zeit[1]), Calculator.getInstance().addZero(zeit[2])};
+        System.out.println("Zeit: " + result[0] + ":" + result[1] + ":" + result[2]);
+    }
+
     /*
-    Rechnet die Zeit in sekunden zu berechnne um.
+    Rechnet die Zeit in Sekunden um.
     @autor Simon
      */
-    public int timeToSek() {
-        int sekHour = this.getHour() * 3600;
-        int sekMin = this.getMin() * 60;
-        int sekSek = this.getSek() + sekHour + sekMin;
-        return sekSek;
+    public int timeToSek(int[] zeit) {
+        if (zeit.length == 3) {
+            int sekHour = zeit[0] * 3600;
+            int sekMin = zeit[1] * 60;
+            return zeit[2] + sekHour + sekMin;
+        } else return 0;
     }
+
     /*
-    Rechnet die gearbeitete Zeit aus mittels Subtraction.
-    Bei Zeit über Mitternacht wird der Ausstemelzeit ein Tag zugerechnet
-    um nich ins minus zu kommen.
+    Rechnet die gearbeitete Zeit aus mittels Subtraktion.
+    Bei Zeit über Mitternacht wird der ausstempel-Zeit ein Tag zugerechnet,
+    damit das Resultat stimmt.
     @autor Simon
      */
     public int calculateTime(int sekStampIn, int sekStampOut) {
         if (sekStampIn > sekStampOut) {
             sekStampOut += 86400;
         }
-        int stampDiff = sekStampOut - sekStampIn;
-        return stampDiff;
+        return sekStampOut - sekStampIn;
     }
 
     /*
-    Rechnet anzahl Sekunden in leserliche Zeit um.
+    Rechnet die Anzahl Sekunden in leserliche Zeit um.
     @autor Simon
      */
-    public void sekToTime(int sekTime) {
+    public int[] sekToTime(int sekTime) {
         int hoursTime;
         int minTime;
         hoursTime = sekTime / 3600;
         sekTime -= 3600 * hoursTime;
         minTime = sekTime / 60;
         sekTime -= 60 * minTime;
-        System.out.println("Zeit gearbeitet: " + hoursTime + ":" + minTime + ":" + sekTime);
-    }
-    public int getHour() {
-        return hour;
-    }
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-    public int getMin() {
-        return min;
-    }
-    public void setMin(int min) {
-        this.min = min;
-    }
-    public int getSek() {
-        return sek;
-    }
-    public void setSek(int sek) {
-        this.sek = sek;
+        return new int[]{hoursTime, minTime, sekTime};
     }
 }
